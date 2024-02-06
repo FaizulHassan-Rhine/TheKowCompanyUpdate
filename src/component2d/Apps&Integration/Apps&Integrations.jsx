f6175993bebceddf5b676f37597e42a37428c999
faizulhassanRien <faizulhassan.rien@gmail.com>
2024-02-06 13:03:40 +0600

    update

--- src/component2d/Apps&Integration/Apps&Integrations.jsx
+++ src/component2d/Apps&Integration/Apps&Integrations.jsx
@@ -17,12 +17,12 @@ const Integrations = () => {
           <div className="flex flex-col md:flex-row justify-center items-center gap-20 md:gap-0 ">
             <a href="https://shopify.thekowcompany.com/Account/Login" target="_blank" rel="noreferrer" className="cursor-pointer ">
             <div className="w-[220px]  py-2 pr-6 md:border-r  md:border-[#B5BCA1]">
-              <img src="/public/img/shopify.png" />
+              <img src="/img/shopify.png" />
             </div>
             </a>
             <a href="https://app.thekowcompany.com/Identity/Account/Login?ReturnUrl=%2F" className="cursor-pointer " target="_blank" rel="noreferrer">
             <div className="w-[160px] pl-6">
-              <img src="/public/img/logo_2.png" />
+              <img src="/img/logo_2.png" />
             </div>
             </a>
           </div>
